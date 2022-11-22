fce92977cd29e87a3e3546a5dc1958a8d72e3334
WeronikaZabrocka <84862493+WeronikaZabrocka@users.noreply.github.com>
2022-11-22 11:31:46 +0100

    zadanka1

--- myscript.js
+++ myscript.js
@@ -1,67 +1,17 @@
-function kalkulator(form) {
-    form.result.innerHTML = kalkulator1(form.l1.value, form.l2.value, form.opt.value);
-}
-
-function kalkulator1(x, y, opt) {
-    x = parseFloat(x);
-    y = parseFloat(y);
-    switch (opt) {
-        case 'dodawanie':
-            return x + y;
-        case 'odejmowanie':
-            return x - y;
-        case 'mnozenie':
-            return x * y;
-        case 'dzielenie':
-            if (y == 0)
-                return 'HEHE bardzo smieszne ale to nie przejdzie';
-            return x / y;
-    }
-}
+const arr1 = ["Anna", "Maria"];
+const arr2 = ["Adam", "Marek", "Bartek", "Tomek"];
+const numbers = [1, 2, 3, 4, 5];
 
 
-
-function palindrom(form) {
-    form.result.innerHTML = palindrom1(form.input.value);
-}
-
-function palindrom1(x) {
-    x = x.toLowerCase();
-    for (let i in x) {
-        if (x[i] != x[x.length - 1 - i])
-            return 'Nie tym razem, próbuj dalej';
-    }
-    return 'Wohooo to palindrom';
-}
+const zad1 = (arr1, arr2) => [...arr1, ...arr2];
+const zad2 = (arr) => arr.reverse()[0];
+const zad3 = (arr, arg) => arr.filter(el1 => el1 > 2);
+const zad4 = (arr) => arr.reduce((previousValue, currectValue) => previousValue + currectValue);
+const zad5 = (arr) => arr.map(n => n * n + 3);
 
 
-function najwieksza(tab) {
-    let najwieksza = 0
-    let druganajwieksza = 0
-    for (i = 0; i < tab.length; i++) {
-        if (tab[i] > najwieksza) {
-            druga_najwieksza = najwieksza;
-            najwieksza = tab[i];
-        }
-    }
-    return druga_najwieksza;
-}
-console.log(najwieksza([1, 1, 2, 3, 4, 5, 5, 6, 8]))
-
-
-
-
-function rekurencja(form) {
-    a = parseInt(form.a.value);
-    b = parseInt(form.b.value);
-    form.result.innerHTML = rekurencja1(a, b);
-}
-
-
-function rekurencja1(a, b) {
-    if (a < b) {
-        return String(a) + " " + String(rekurencja1(a + 1, b))
-    } else {
-        return a
-    }
-}
+console.log(zad1(arr1, arr2));
+console.log(zad2(arr2));
+console.log(zad3(numbers));
+console.log(zad4(numbers));
+console.log(zad5(numbers));
